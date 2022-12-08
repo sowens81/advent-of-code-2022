33791490a592835e86c903763ff396c5208153d2
Steve Owens <stevejowens81@outlook.com>
2022-12-08 14:49:29 +0000

    Day 4 completed

--- Day 4/task2.py
+++ Day 4/task2.py
@@ -4,16 +4,18 @@ file = open("data.txt")
 lines = file.readlines()
 pairs = [line.strip() for line in lines]
 
-def validate_range(a, b):
+def validate_overlaps(a, b):
     startA, endA = map(int, a.split('-'))
     startB, endB = map(int, b.split('-'))
-    return startB <= startA and endA <= endB
+    
+    return startB <= endA and startA <= endB
+
 
 for index, p in enumerate(pairs):
     workingList = []
     a, b = p.split(',')
     print("line:" + str(index+1))
-    if validate_range(a, b) or validate_range(b, a):
+    if validate_overlaps(a, b) or validate_overlaps(b, a):
         print("Bad line:" + str(index+1))
         errorLines += 1
             
